d9aeeaad95d9affc9d11964f5220e6ca21b5471e
Jakub <jakub@cubepotato.eu>
2022-04-02 14:33:02 +0200

    ++

--- server/src/placeCanvas.ts
+++ server/src/placeCanvas.ts
@@ -4,6 +4,7 @@ const canvasJsPath = 'document.querySelector("body > mona-lisa-app > faceplate-c
 
 let browser: puppeteer.Browser;
 let page: puppeteer.Page;
+let busy = false;
 
 export async function getPixelsAt(x: number, y: number, w: number, h: number): Promise<ImageData> {
     if(!browser) {
@@ -17,6 +18,10 @@ export async function getPixelsAt(x: number, y: number, w: number, h: number): P
         page = await browser.newPage();
     }
 
+    if(busy) return;
+
+    busy = true;
+
     page.goto('https://www.reddit.com/r/place/?cx=500&cy=500&px=460');
     await page.waitForSelector('.moeaZEzC0AbAvmDwN22Ma');
     await page.click('.moeaZEzC0AbAvmDwN22Ma');
@@ -28,5 +33,8 @@ export async function getPixelsAt(x: number, y: number, w: number, h: number): P
 
     const canvas = await (await frame.evaluateHandle(canvasJsPath)).asElement()!;
     const data = await canvas.evaluate((c: HTMLCanvasElement, [x, y, w, h]) => c.getContext('2d').getImageData(x, y, w, h), [x, y, w, h]);
+
+    busy = false;
+
     return data;
 }
